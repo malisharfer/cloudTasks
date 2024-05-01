9e54fecb14b360d823407a0c1d0e3bd452bf64dc
MaliSharfer <malis@skyvar.co.il>
2024-05-01 10:16:48 +0300

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,18 +3,6 @@ variable subscription_id {
   type        = string
 }
 
-variable DOCKER_REGISTRY_SERVER_URL {
-  type        = string
-}
-
-variable DOCKER_REGISTRY_SERVER_USERNAME {
-  type        = string
-}
-
-variable DOCKER_REGISTRY_SERVER_PASSWORD {
-  type        = string
-}
-
 # end secrets
 
 variable rg_name {
