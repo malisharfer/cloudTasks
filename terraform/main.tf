1500b60e92794fe82f3307f206c41087f8a4495f
MaliSharfer <malis@skyvar.co.il>
2024-05-02 08:58:01 +0300

    first commit555

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "service_plan" {
 }
 
 resource "azurerm_linux_function_app" "linux_function_app" {
-  name                        = "func-try-deploy-acr-2"
+  name                        = "func-try-deploy-acr-3"
   location                    = data.azurerm_storage_account.storage_account.location
   resource_group_name         = data.azurerm_storage_account.storage_account.resource_group_name
   service_plan_id             = azurerm_service_plan.service_plan.id
@@ -42,28 +42,61 @@ resource "azurerm_linux_function_app" "linux_function_app" {
 
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "python"
+    https_only = true
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
   }
 
   site_config {
     always_on = true
-    container_registry_use_managed_identity = true
-    application_stack {
-      docker {
-        registry_url = var.DOCKER_REGISTRY_SERVER_URL
-        image_name = var.IMAGE_NAME
-        image_tag = var.IMAGE_TAG
-        registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
-        registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
-      }
-    }
+    # application_stack {
+    #   docker {
+    #     registry_url = var.DOCKER_REGISTRY_SERVER_URL
+    #     image_name = var.IMAGE_NAME
+    #     image_tag = var.IMAGE_TAG
+    #     registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
+    #     registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
+    #   }
+    # }
   }
-
 
   identity {
     type = "SystemAssigned"
   }
 }
+
+# resource "azurerm_linux_function_app" "linux_function_app" {
+#   name                        = "func-try-deploy-acr-2"
+#   location                    = data.azurerm_storage_account.storage_account.location
+#   resource_group_name         = data.azurerm_storage_account.storage_account.resource_group_name
+#   service_plan_id             = azurerm_service_plan.service_plan.id
+#   storage_account_name        = data.azurerm_storage_account.storage_account.name
+#   storage_account_access_key  = data.azurerm_storage_account.storage_account.primary_access_key
+#   functions_extension_version = "~4"
+
+#   app_settings = {
+#     FUNCTIONS_WORKER_RUNTIME = "python"
+#     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+#   }
+
+#   site_config {
+#     always_on = true
+#     0000 = true
+#     application_stack {
+#       docker {
+#         registry_url = "https://containerregistryautomationdev.azurecr.io"
+#         image_name = "services/try1/func_user_disable"
+#         image_tag = "3.0.0"
+#         # registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
+#         # registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
+#       }
+#     }
+#   }
+
+
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
 data "azurerm_client_config" "current_client" {}
 
