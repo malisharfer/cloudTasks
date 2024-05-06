20bb53da400b2839b3fa0228e426782cd84eaf4a
MaliSharfer <malis@skyvar.co.il>
2024-05-06 08:38:58 +0300

    first commit555

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,6 @@ resource "azurerm_linux_function_app" "linux_function_app" {
     always_on = false
     container_registry_use_managed_identity = true
     application_stack {
-      # python_version = "3.10"
       docker {
         registry_url = "https://containerregistryautomationdev.azurecr.io"
         image_name = "services/try1/func_user_disable"
@@ -63,40 +62,6 @@ resource "azurerm_linux_function_app" "linux_function_app" {
   }
 }
 
-# resource "azurerm_linux_function_app" "linux_function_app" {
-#   name                        = "func-try-deploy-acr-2"
-#   location                    = data.azurerm_storage_account.storage_account.location
-#   resource_group_name         = data.azurerm_storage_account.storage_account.resource_group_name
-#   service_plan_id             = azurerm_service_plan.service_plan.id
-#   storage_account_name        = data.azurerm_storage_account.storage_account.name
-#   storage_account_access_key  = data.azurerm_storage_account.storage_account.primary_access_key
-#   functions_extension_version = "~4"
-
-#   app_settings = {
-#     FUNCTIONS_WORKER_RUNTIME = "python"
-#     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
-#   }
-
-#   site_config {
-#     always_on = true
-#     0000 = true
-#     application_stack {
-#       docker {
-#         registry_url = "https://containerregistryautomationdev.azurecr.io"
-#         image_name = "services/try1/func_user_disable"
-#         image_tag = "3.0.0"
-#         # registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
-#         # registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
-#       }
-#     }
-#   }
-
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
-
 data "azurerm_client_config" "current_client" {}
 
 data "azurerm_container_registry" "example" {
@@ -105,10 +70,8 @@ data "azurerm_container_registry" "example" {
 }
 
 resource "azurerm_role_assignment" "example" {
-  # object_id    = azurerm_linux_function_app.linux_function_app.identity.principal_id
   principal_id                     = azurerm_linux_function_app.linux_function_app.identity[0].principal_id
   role_definition_name             = "AcrPull"
   scope                            = data.azurerm_container_registry.example.id
   skip_service_principal_aad_check = true
 }
-
