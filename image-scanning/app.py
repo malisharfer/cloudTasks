9edaea73691eb483c4d33b3de045c3165d1bcce1
MaliSharfer <malis@skyvar.co.il>
2024-06-04 08:33:11 +0300

    image-scanning

--- image-scanning/app.py
+++ image-scanning/app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 from image_scanning import run_resource_graph_query
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    return 'Hello, World! image-scanning'
 
 @app.route("/image_push_acr", methods=["POST"])
 def add_new_user():
     respons = request.get_json()
     run_resource_graph_query(respons["rg_name"],respons["digest"],respons["date"])
-    return "ok." 
+    return respons
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8080)
