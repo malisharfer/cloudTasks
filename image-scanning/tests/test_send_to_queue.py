af6bc6cd57ada8f78d1d0b594f3efb2c33a8588e
MaliSharfer <malis@skyvar.co.il>
2024-06-24 14:03:00 +0300

    Ffff

--- image-scanning/tests/test_send_to_queue.py
+++ image-scanning/tests/test_send_to_queue.py
@@ -1,8 +1,7 @@
 from unittest.mock import patch, MagicMock
 import json
 from datetime import datetime
-from project.image_scanning import send_to_queue
-
+from project.image_scanning import send_to_queue , set_resource_graph_query
 
 @patch("azure.storage.queue.QueueClient.from_connection_string")
 def test_send_to_queue(mock_from_connection_string):
@@ -20,3 +19,10 @@ def test_send_to_queue(mock_from_connection_string):
 
     expected_message = json.dumps({"key": "value", "dateOfPush": date})
     mock_queue_client_instance.send_message.assert_called_once_with(expected_message)
+
+def test_set_resource_graph_query():
+    resource_group_name = 'test_resource_group'
+    image_digest = 'test_image_digest'
+    actual_query = set_resource_graph_query(resource_group_name, image_digest) 
+    assert isinstance(actual_query, str)
+
