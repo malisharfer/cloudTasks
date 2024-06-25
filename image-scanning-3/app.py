843a0d8f964de2bc2fad0356b0dd8db85c405216
MaliSharfer <malis@skyvar.co.il>
2024-06-25 09:31:59 +0300

    get-request

--- image-scanning-3/app.py
+++ image-scanning-3/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request
 from waitress import serve
-from project.image_scanning import run_resource_graph_query
+from project.image_scanning import run_resource_graph_query ,send_to_queue
 
 app = Flask(__name__)
 
+@app.route("/try_get")
+def try_get():
+    send_to_queue()
+    return "good!!!"
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
