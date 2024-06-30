51b10c3c266aebeecb8c474d5793eaa2cd07cd8c
MaliSharfer <malis@skyvar.co.il>
2024-06-30 08:55:20 +0300

    get-request

--- image-scanning-5/app.py
+++ image-scanning-5/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from waitress import serve
 from project.image_scanning import run_resource_graph_query ,send_to_queue
 
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
-    response = request.get_json()
-    # run_resource_graph_query(response["rg_name"], response["digest"], response["date"])
-    send_to_queue("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
-    return response
+    data = request.json 
+    print("Received webhook data:", data)
+    send_to_queue(data)
+    return jsonify({'message': 'Webhook received successfully'}), 200
+    # response = request.get_json()
+    # # run_resource_graph_query(response["rg_name"], response["digest"], response["date"])
+    # send_to_queue("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+    # return response
 
 
 if __name__ == "__main__":
