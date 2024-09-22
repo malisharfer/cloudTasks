81b1f9aa46cc94ba68fe53f9828ef28eb9adac35
MaliSharfer <malis@skyvar.co.il>
2024-09-22 12:55:13 +0300

    push to deploy

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
-
-class User extends Authenticatable implements HasName
+class User extends Authenticatable implements HasName,FilamentUser
 {
     use HasFactory, HasPermissions, HasRoles, Notifiable;
 
@@ -38,13 +38,14 @@ class User extends Authenticatable implements HasName
 
     public function displayName(): Attribute
     {
-        return Attribute::get(fn() => $this->last_name . ', ' . $this->first_name);
+        return Attribute::get(fn () => $this->last_name.', '.$this->first_name);
     }
 
     public function getFilamentName(): string
     {
         return $this->displayName;
     }
+
     public function viewAny(User $user): bool
     {
         return $user->hasRole('manager');
