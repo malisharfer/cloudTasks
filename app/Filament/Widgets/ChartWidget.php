829d6f06e69452cff85828b5588223500f6e29ac
MaliSharfer <malis@skyvar.co.il>
2025-06-22 11:27:09 +0300

    push

--- app/Filament/Widgets/ChartWidget.php
+++ app/Filament/Widgets/ChartWidget.php
@@ -2,181 +2,69 @@
 
 namespace App\Filament\Widgets;
 
-use App\Enums\MonthesInYear;
-use App\Models\Soldier;
-use App\Services\Charts;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Radio;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Toggle;
-use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+use Filament\Support\RawJs;
+use Filament\Widgets\BarChartWidget;
+use Illuminate\Support\Collection;
+use Illuminate\Contracts\Support\Htmlable;
 
-class ChartWidget extends ApexChartWidget
+
+class ChartWidget extends BarChartWidget
 {
-    protected static ?int $sort = 3;
+    protected static ?string $heading = null;
 
-    protected static ?int $contentHeight = 500;
+    protected static ?string $pollingInterval = '0';
+    
+    public ?Collection $data;
 
-    protected function getFormSchema(): array
+    public ?string $headerLable;
+    
+
+    public function getHeading(): string | Htmlable | null
     {
-        return [
-            Radio::make('ChartType')
-                ->default('bar')
-                ->options([
-                    'line' => __('Line'),
-                    'bar' => __('Col'),
-                    'area' => __('Area'),
-                ])
-                ->inline(true)
-                ->label(__('Type')),
-            Grid::make()
-                ->schema([
-                    Toggle::make('ordersChartMarkers')
-                        ->default(false)
-                        ->label(__('Markers')),
+        return $this->headerLable;
+    }
 
-                    Toggle::make('ordersChartGrid')
-                        ->default(false)
-                        ->label(__('Grid')),
-                ]),
-            Select::make('data')
-                ->default('points')
-                ->hiddenLabel()
-                ->placeholder(__('Select parameter to filter'))
-                ->live()
-                ->options([
-                    'shifts' => __('Shifts'),
-                    'lowConstraintsRejected' => __('low Constraints Rejected'),
-                    'constraints' => __('Constraints'),
-                    'points' => __('Points'),
-                    'weekends' => __('Weekends'),
-                    'nights' => __('Nights'),
-                ]),
-            Select::make('year')
-                ->default(now()->year)
-                ->label(__('Year'))
-                ->options($this->getYearOptions())
-                ->placeholder(__('Select year')),
-            Select::make('month')
-                ->default(12)
-                ->label(__('Month'))
-                ->options($this->getMonthOptions())
-                ->placeholder(__('Select month')),
-            Select::make('course')
-                ->default(1)
-                ->options(Soldier::select('course')->distinct()->orderBy('course')->pluck('course', 'course')->all())
-                ->placeholder(__('Select course'))
-                ->label(__('Course')),
+    protected function getData(): array
+    {
+        $datasets = collect();
+        $colors = ['#36A2EB', '#dc38e4', '#FF6384', '#FFCE56', '#36A2EB'];
+    
+        $this->data->each(function ($soldiers, $key) use ($datasets, $colors) {
+            $datasets->push([
+                'label' => 'Max '. (string)$key,
+                'data' => $soldiers->values()->all(),
+                'backgroundColor' => collect(range(0, $soldiers->count() - 1))->map(function () use ($datasets, $colors) {
+                    return $colors[$datasets->count() % count($colors)];
+                })->all(), 
+                'borderColor' => $colors[$datasets->count() % count($colors)], 
+                'labels' => $soldiers->keys()->all(),
+            ]);
+        });
+    
+        return [
+            'datasets' => $datasets->all(),
+            'labels' => collect(range(0, $this->data->max(fn($soldiers) => $soldiers->count()) - 1))->map(fn() => '')->all(),
         ];
     }
 
-    protected function getOptions(): array
+    protected function getOptions(): array|RawJs|null
     {
-        $filters = $this->filterFormData;
-        $chart = new Charts;
-        $detailes = $chart->organizeChartData($filters['data'], $filters['course'], $filters['month'], $filters['year']);
-
-        return [
-            'chart' => [
-                'type' => $filters['ChartType'],
-                'height' => 490,
-                'toolbar' => [
-                    'show' => true,
-                ],
-            ],
-            'theme' => [
-                'mode' => 'light',
-            ],
-            'series' => [
-                [
-                    'name' => __('label', ['data' => __($filters['data']), 'course' => $filters['course']]),
-                    'data' => $detailes['data'],
-                ],
-            ],
-            'plotOptions' => [
-                'bar' => [
-                    'borderRadius' => 2,
-                ],
-            ],
-            'xaxis' => [
-                'categories' => $detailes['labels'],
-                'labels' => [
-                    'show' => false,
-                ],
-            ],
-            'tooltip' => [
-                'enabled' => true,
-                'x' => [
-                    'formatter' => function ($val, $index) use ($detailes) {
-                        return $detailes['labels'][$index] ?? $val;
-                    },
-                ],
-            ],
-            'yaxis' => [
-                'labels' => [
-                    'style' => [
-                        'fontWeight' => 400,
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'fill' => [
-                'type' => 'gradient',
-                'gradient' => [
-                    'shade' => 'dark',
-                    'type' => 'vertical',
-                    'shadeIntensity' => 0.5,
-                    'gradientToColors' => ['#fbbf24'],
-                    'inverseColors' => true,
-                    'opacityFrom' => 1,
-                    'opacityTo' => 1,
-                    'stops' => [0, 100],
-                ],
-            ],
-
-            'dataLabels' => [
-                'enabled' => false,
-                'dropShadow' => true,
-            ],
-            'grid' => [
-                'show' => $filters['ordersChartGrid'],
-            ],
-            'tooltip' => [
-                'enabled' => true,
-            ],
-            'stroke' => [
-                'width' => $filters['ChartType'] === 'line' ? 4 : 0,
-            ],
-            'noData' => [
-                'text' => __('No matching data!'),
-                'align' => 'center',
-                'verticalAlign' => 'middle',
-                'offsetX' => 0,
-                'offsetY' => 0,
-                'style' => [
-                    'color' => '#FF0000',
-                    'fontSize' => '17px',
-                    'fontWeight' => 400,
-                    'fontFamily' => 'inherit',
-                ],
-            ],
-            'colors' => ['#f59e0b'],
-        ];
-    }
-
-    protected function getYearOptions(): array
-    {
-        $currentYear = now()->year;
-        $years = range($currentYear - 1, $currentYear + 4);
-
-        return array_combine($years, $years);
-    }
-
-    protected function getMonthOptions(): array
-    {
-        return array_combine(
-            array_map(fn ($enum) => $enum->value, MonthesInYear::cases()),
-            array_map(fn ($enum) => $enum->getLabel(), MonthesInYear::cases())
-        );
+        return RawJs::make(<<<JS
+        {
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            const soldierIndex = context.dataIndex; 
+                            const soldierName = context.dataset.labels ? context.dataset.labels[soldierIndex] : 'Unknown Soldier';
+                            const value = context.parsed.y; 
+    
+                            return soldierName + ': ' + value;
+                        }
+                    }
+                }
+            }
+        }
+        JS);
     }
 }
--- app/Resources/ChartResource/Pages/ListCharts.php
+++ app/Resources/ChartResource/Pages/ListCharts.php
@@ -12,8 +12,98 @@ class ListCharts extends ListRecords
 
     protected function getHeaderWidgets(): array
     {
+
         return [
-            ChartWidget::make(),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('Points')
+            ]),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('Weekends')
+            ]),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('Nights')
+            ]),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('Regulars')
+            ]),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('Alerts')
+            ]),
+            ChartWidget::make([
+                'data'=> collect([
+                    5.5 => collect([
+                        'Dan' => 3,
+                        'John' => 3,
+                        'Gal' => 4.25,
+                        'Or' => 5,
+                    ]),
+                    3.25 => collect([
+                        'Yossi' => 3,
+                        'Mor' => 3.05,
+                    ])
+                ]),
+                'headerLable'=>__('In parallels')
+            ]),
         ];
     }
 }
