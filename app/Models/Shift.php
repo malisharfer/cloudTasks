c91a73fc6e4d091ac971b43d7dae74eae51d7844
MaliSharfer <malis@skyvar.co.il>
2025-07-24 10:04:55 +0300

    last change

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -120,7 +120,7 @@ class Shift extends Model
                         fn (?Shift $record): bool => $record !== null
                         && Carbon::parse($record->start_date)->isAfter(now())
                         && ! $record->soldier_id
-                        && \Str::contains($_SERVER['HTTP_REFERER'], 'my-soldiers-shifts')
+                        && \Str::contains($_SERVER['HTTP_REFERER'], 'all-shifts')
                         && current(array_diff(collect(auth()->user()->getRoleNames())->toArray(), ['soldier']))
                     )
                     ->hiddenOn('view'),
